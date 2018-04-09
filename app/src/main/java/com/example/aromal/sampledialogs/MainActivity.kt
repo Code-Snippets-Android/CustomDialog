aa03bebea461c6410ff539794186ceaf14ca0ef6
Aromal <aromalsasidharan@gmail.com>
2018-04-09 22:15:54 +0530

    dialog width in window adjusted

--- app/src/main/java/com/example/aromal/sampledialogs/MainActivity.kt
+++ app/src/main/java/com/example/aromal/sampledialogs/MainActivity.kt
@@ -22,41 +22,49 @@ import kotlinx.android.synthetic.main.dialog_view.view.*
 class DialogView  @JvmOverloads constructor(
         context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : RelativeLayout(context, attrs, defStyleAttr) {
+    var view:View? = null
     private val compositeDisposable:CompositeDisposable = CompositeDisposable()
     public interface DialogViewListner{
         fun onOkClicked(dialog:Dialog)
     }
     var listner:DialogViewListner? = null
     private val dialog:Dialog by lazy {
-        Dialog(context)
+        Dialog(context,R.style.DialogTheme)
     }
     init {
-        LayoutInflater.from(context).inflate(R.layout.dialog_view,this,true)
+        view = LayoutInflater.from(context).inflate(R.layout.dialog_view,this,true)
         this.textView.text = "Aromal"
         this.setupView()
+
     }
 
     fun setupView(){
         dialog.setContentView(this)
-
+        setupShow()
         RxView.clicks(this.okButton).subscribe {
             this.listner?.onOkClicked(this.dialog)
 
         }.addTo(compositeDisposable)
+
+//        RxView.clicks(this.view!!).subscribe {
+//           dismiss()
+//
+//        }.addTo(compositeDisposable)
+
     }
     fun dismiss(){
         this.dialog.dismiss()
     }
-    private fun setupAfterShow(){
-//        dialog.setCancelable(true)
+    private fun setupShow(){
+        dialog.setCancelable(true)
         dialog.setCanceledOnTouchOutside(true)
-//        dialog.window.setLayout(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+
     }
     fun show(dialogViewListner: DialogViewListner) {
         this.listner = dialogViewListner
         if (!dialog.isShowing) {
             this.dialog.show()
-            this.setupAfterShow()
+
         }
     }
 
